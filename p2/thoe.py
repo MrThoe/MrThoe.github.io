486b6a46c3a3cdf35676c8ba76eae6b0b7effb59
MrThoe <allen_thoe@redlandsschools.net>
2018-09-17 15:51:54 +0000

    Changed the sideright padding

---
+++ p2/thoe.py
@@ -0,0 +1,4 @@
+print ("hello world")
+
+age = input('how old are you?')
+print (2*age)
---
+++ two.py
@@ -0,0 +1,7 @@
+print ("hello world")
+
+age = int(input('how old are you?'))
+print (2*age)
+
+print ("Wow, you are " + str(age) + "!")
+input()
